5c97eaa5119a2141f4602e857c7129156bc5cea2
Abhinav <abhinav07c@gmail.com>
2024-03-30 12:30:27 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,29 +1,31 @@
+import React, { useState } from 'react';
+import { slide as Menu } from 'react-burger-menu';
 import './Navbar.css';
-import thelogo from '../assets/lejhro_logo_white.png'
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="container">
-      <nav className="navbar navbar-expand-lg">
-        <ul className="nav justify-content-center">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/innovations">Innovations</a>
-          </li>
-          <div class="vr"></div>
-          <li className="nav-item">
-            <a className="nav-link" href="/business-services">Business Services</a>
-          </li>
-          <div class="vr"></div>
-          <li className="nav-item">
-            <a className="nav-link" href="/financial-services">Financial Services</a>
-          </li>
-          <div class="vr"></div>
-          <li className="nav-item">
-            <a className="nav-link" href="http://www.bootcamp.lejhro.com/" target="_blank" rel="noreferrer">Bootcamp</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="navbar container text-center">
+
+      <ul className="nav justify-content-center">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/innovations">Innovations</a>
+        </li>
+        <div className="vr"></div>
+        <li className="nav-item">
+          <a className="nav-link" href="/business-services">Business Services</a>
+        </li>
+        <div className="vr"></div>
+        <li className="nav-item">
+          <a className="nav-link" href="/financial-services">Financial Services</a>
+        </li>
+        <div className="vr"></div>
+        <li className="nav-item">
+          <a className="nav-link" href="http://www.bootcamp.lejhro.com/" target="_blank" rel="noreferrer">Bootcamp</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
